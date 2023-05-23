a38cd5eaa4844aa21b8061b0487ee30490eeaef6
Nidhi Pabbathi <nidhi.pabbathi@utexas.edu>
2023-05-23 01:31:27 -0500

    added color assets and started list view for the project gallery

--- Crocheto/ContentView.swift
+++ Crocheto/ContentView.swift
@@ -10,43 +10,69 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack {
+                Button(action: addItem) {
+                    Label("Add Project", systemImage: "plus")
+                        .padding()
+                }.tint(CrochetoColors.white)
+                    .background(CrochetoColors.green)
+                
+                    .cornerRadius(15)
+                    .padding()
+                List {
+                    ForEach(items) { item in
+                        Section {
+                            VStack {
+                                NavigationLink {
+                                    newGalleryProject()
+                                } label: {
+                                    Image(systemName: "globe")
+                                        .listRowBackground(Color.pink)
+                                        .padding()
+                                    Spacer()
+                                    Text("Title")
+                                        .lineLimit(1)
+                                        .bold()
+                                    Spacer()
+                                    Text("Project description that can extend multiple lines")
+                                        .lineLimit(2)
+                                        .padding()
+                                }.foregroundColor(CrochetoColors.green)
+                            }.overlay(
+                                RoundedRectangle(cornerRadius: 10, style: .circular).stroke(CrochetoColors.green, lineWidth: 10)
+                            )
+                        }.listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
                     }
-                }
-                .onDelete(perform: deleteItems)
+                    .onDelete(perform: deleteItems)
+                    .listRowBackground(CrochetoColors.white)
+                    
+                }.listStyle(.insetGrouped)
+                
+//                .shadow(color: CrochetoColors.lightGreen, radius: 10)
+//                        width: Text("Title")
+                    .lineLimit(1)
+                    .bold()
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+                .background(CrochetoColors.darkGreen)
+                .scrollContentBackground(.hidden)
+                
+                
         }
     }
-
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -57,11 +83,11 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
---
+++ Crocheto/CrochetoColors.swift
@@ -0,0 +1,16 @@
+//
+//  CrochetoColors.swift
+//  Crocheto
+//
+//  Created by Nidhi Pabbathi on 5/23/23.
+//
+
+import SwiftUI
+
+struct CrochetoColors {
+    static let darkGreen = Color("Dark Green")
+    static let green = Color("Green")
+    static let lightGreen = Color("Light Green")
+    static let white = Color("Off White")
+    static let brown = Color("Brown")
+}
